4aa1e099b5d521a7e1ddd7ccc18115443b2ff0ab
Ayon95 <mushfiqurrahman78@yahoo.com>
2021-11-19 23:46:00 -0500

    added async await example in promise.js, and async/await explanation in questions.txt

--- JavaScript/asynchronous/promise.js
+++ JavaScript/asynchronous/promise.js
@@ -3,3 +3,16 @@ fetch("https://jsonplaceholder.typicode.com/users")
   .then((response) => response.json())
   .then((data) => console.log(data))
   .catch((error) => console.log(error.message));
+
+// using async/await syntax
+async function getUsers() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+getUsers();
