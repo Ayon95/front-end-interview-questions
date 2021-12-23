21c3494282e76a5d6c581eac4c8fe18baefe3dd0
Ayon95 <mushfiqurrahman78@yahoo.com>
2021-12-23 00:36:20 -0500

    added some more information regarding event delegation and propagation

--- JavaScript/eventDelegation/eventDelegation.js
+++ JavaScript/eventDelegation/eventDelegation.js
@@ -1,7 +1,5 @@
-const messageDisplayArea = document.querySelector(".message-display");
 const itemsList = document.querySelector("ul");
 function handleClick(e) {
-  console.log("called");
   const targetElement = e.target;
   // only do something if the click event was triggered by a button
   if (!targetElement.classList.contains("btn")) return;
